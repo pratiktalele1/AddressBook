7cd69b2acffc4617a8da90b289f812c6deb6e413
Pratik Talele <pratiktalele22@gmail.com>
2021-08-02 14:51:07 +0530

    commiting UC1 adding data to address book

--- src/Classes/AddressBookMain.java
+++ src/Classes/AddressBookMain.java
@@ -1,7 +1,15 @@
 package Classes;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddressBookMain {
-public static void main(String[] args) {
-	System.out.println("welcome to address book project");
-}
+	public static void main(String[] args) {
+		//creating object
+		UC1 uc1=new UC1();
+		//adding data to hash map
+		uc1.add();
+		//printing data from collection
+		uc1.print();
+	}
 }
