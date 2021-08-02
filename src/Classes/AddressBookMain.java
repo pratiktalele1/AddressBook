abdc895bc5bf02fb4d5c5f4f2a479bb63d4cb240
Pratik Talele <pratiktalele22@gmail.com>
2021-08-02 15:01:16 +0530

    commiting UC2

--- src/Classes/AddressBookMain.java
+++ src/Classes/AddressBookMain.java
@@ -2,14 +2,27 @@ package Classes;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class AddressBookMain {
 	public static void main(String[] args) {
+		int i=0;
 		//creating object
 		UC1 uc1=new UC1();
 		//adding data to hash map
 		uc1.add();
 		//printing data from collection
 		uc1.print();
+		
+		UC2 uc2=new UC2();
+		do {
+			uc2.add();
+			System.out.print("0 =>to end && 1=>to again ====> ");
+			Scanner scanInput = new Scanner(System.in);
+			i = scanInput.nextInt();
+			
+		}while(i==1);
+		
+		uc2.print();
 	}
 }
---
+++ src/Classes/UC2.java
@@ -0,0 +1,5 @@
+package Classes;
+
+public class UC2 extends UC1 {
+
+}
