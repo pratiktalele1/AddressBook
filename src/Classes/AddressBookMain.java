cd091790bbcfcf5cedd5d569999178b9eb36adf4
Pratik Talele <pratiktalele22@gmail.com>
2021-08-02 16:32:10 +0530

    commiting UC5 multiple user

--- src/Classes/AddressBookMain.java
+++ src/Classes/AddressBookMain.java
@@ -12,13 +12,15 @@ public class AddressBookMain {
 		UC2 uc2 = new UC2();
 		UC3 uc3 = new UC3();
 		UC4 uc4 = new UC4();
+		UC5 uc5 = new UC5();
 		// adding data to hash map
 		// uc1.add();
 		// printing data from collection
 		// uc1.print();
 
 		do {
-			System.out.print("1=>add contact\t 2=>edit contact\t 3=>delete contact \t 4=>display all ");
+			System.out.print(
+					"1=>add contact\t 2=>edit contact\t 3=>delete contact \t 4=>display all \t 5=>add multiple contact");
 			Scanner scanInput1 = new Scanner(System.in);
 			int getUserInput = scanInput1.nextInt();
 
@@ -38,6 +40,15 @@ public class AddressBookMain {
 			}
 			case 4: {
 				uc2.print();
+				break;
+			}
+			case 5: {
+				System.out.print("Enter first number- ");
+				Scanner scanInput = new Scanner(System.in);
+				int getUserInput2 = scanInput.nextInt();
+				for (int j = 0; j < getUserInput2; j++) {
+					uc5.add();
+				}
 				break;
 			}
 			}
