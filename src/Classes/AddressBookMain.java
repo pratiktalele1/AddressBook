a593acf66a47bdcece1d199121e86179313063ae
Pratik Talele <pratiktalele22@gmail.com>
2021-08-02 16:09:25 +0530

    commiting UC4

--- src/Classes/AddressBookMain.java
+++ src/Classes/AddressBookMain.java
@@ -6,28 +6,47 @@ import java.util.Scanner;
 
 public class AddressBookMain {
 	public static void main(String[] args) {
-		int i=0;
-		//creating object
-		UC1 uc1=new UC1();
-		//adding data to hash map
-		uc1.add();
-		//printing data from collection
-		uc1.print();
-		
-		UC2 uc2=new UC2();
+		int i = 0;
+		// creating object
+		UC1 uc1 = new UC1();
+		UC2 uc2 = new UC2();
+		UC3 uc3 = new UC3();
+		UC4 uc4 = new UC4();
+		// adding data to hash map
+		// uc1.add();
+		// printing data from collection
+		// uc1.print();
+
 		do {
-			uc2.add();
+			System.out.print("1=>add contact\t 2=>edit contact\t 3=>delete contact \t 4=>display all ");
+			Scanner scanInput1 = new Scanner(System.in);
+			int getUserInput = scanInput1.nextInt();
+
+			switch (getUserInput) {
+			case 1: {
+				uc2.add();
+				break;
+			}
+			case 2: {
+				uc3.editExisting();
+				uc3.print();
+				break;
+			}
+			case 3: {
+				uc4.deleteContact();
+				break;
+			}
+			case 4: {
+				uc2.print();
+				break;
+			}
+			}
+
 			System.out.print("0 =>to end && 1=>to again ====> ");
 			Scanner scanInput = new Scanner(System.in);
 			i = scanInput.nextInt();
-			
-		}while(i==1);
-		
-		uc2.print();
-		
-		UC3 uc3=new UC3();
-		uc3.editExisting();
-		
-		uc3.print();
+
+		} while (i == 1);
+
 	}
 }
